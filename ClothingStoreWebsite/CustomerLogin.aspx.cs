7e0b9ca2c2189a1b3a919aa3c489a26776ddde6b
Niruddya Samarasinghe <niruddya.samarasinghe@gmail.com>
2020-10-02 11:52:44 +0530

    ASP .NET application for clothing store

---
+++ ClothingStoreWebsite/CustomerLogin.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClothingStoreWebsite
+{
+    public partial class CustomerLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SignUp(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.SQLEXPRESS;AttachDbFilename=|DataDirectory|ClothingDB.mdf;Integrated Security=True;User Instance=True");
+
+            SqlCommand cmd = new SqlCommand("select * from CustomerDetails where username=@username and word=@word", con);
+            cmd.Parameters.AddWithValue("@username", username.Text);
+            cmd.Parameters.AddWithValue("@pass", pass.Text);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (dt.Rows.Count > 0)
+            {
+                Response.Redirect("home-02.html");
+            }
+        }
+        }
+}
